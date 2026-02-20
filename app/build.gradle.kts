2946d1ee565558c975e162a2db0ae7a2adbfdfe7
dgddgd314 <dgddgd314@gmail.com>
2026-02-20 23:39:58 +0900

    feature: add google maps dependency and implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,6 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.56.2")
     ksp("com.google.dagger:hilt-android-compiler:2.56.2")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("com.google.maps.android:maps-compose:4.3.3")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
 }
