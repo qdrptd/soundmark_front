8fab95109a47a9fb296790da32c18d297a50f5be
qdrptd <qdrptd@gmail.com>
2026-02-22 05:32:01 +0900

    온보드 디자인

--- app/src/main/java/com/example/soundmark/ui/views/onboard/OnboardScreen.kt
+++ app/src/main/java/com/example/soundmark/ui/views/onboard/OnboardScreen.kt
@@ -4,14 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,7 +31,9 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.soundmark.R
 import com.example.soundmark.ui.theme.BackgroundDark
+import com.example.soundmark.util.bold
 import com.example.soundmark.util.color
+import com.example.soundmark.util.size
 
 @Composable
 fun OnboardScreen(
@@ -96,8 +102,26 @@ fun LoginButton(
         onClick = onClick,
         modifier = modifier
     ) {
-        Text(text = "Spotify로 로그인하기", style = MaterialTheme.typography.bodyMedium.color(
-            BackgroundDark
-        ))
+        Row(
+            modifier = Modifier.padding(vertical = 6.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Icon(
+                painter = painterResource(R.drawable.icon_spotify), // svg xml
+                contentDescription = null,
+                tint = Color.Unspecified, // 원본 색 유지 (중요)
+                modifier = Modifier.size(32.dp)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = "Spotify로 로그인하기",
+                style = MaterialTheme.typography.bodyMedium.size(18).bold(),
+                color = BackgroundDark
+            )
+        }
     }
 }
